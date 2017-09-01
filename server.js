bc7f601dce29d8311c05ed095ecad6245857c8da
Tarun <tarun.softengg@gmail.com>
2017-09-01 10:13:19 +0530

    HMR removed for routes issue

--- server.js
+++ server.js
@@ -7,22 +7,9 @@ const indexPath = path.join(__dirname, './public/index.html')
 const publicPath = express.static(path.join(__dirname, './public'))
 
 app.use('/public', publicPath)
+
 app.get('*', function (req, res) { res.sendFile(indexPath) });
-
-const port = (process.env.PORT || 8080)
-if (process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack')
-  const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackHotMiddleware = require('webpack-hot-middleware')
-  const config = require('./webpack.dev.config.js')
-  const compiler = webpack(config)
-
-  app.use(webpackHotMiddleware(compiler))
-  app.use(webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath: './public'
-  }))
-}
+const port = (process.env.PORT || 3014);
 
 app.listen(port)
 console.log(`Listening at http://localhost:${port}`)
